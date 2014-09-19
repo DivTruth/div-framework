59111f6bcbd810f401e2c064a5dd99cb891cc852
Nick Worth <nworth@raprec.com>
2014-09-19 14:50:02 -0700

    remove font-awesome from post content

--- parts/post/loop-post-info.php
+++ parts/post/loop-post-info.php
@@ -4,11 +4,9 @@ ob_start();
 comments_number( '0 Comments', '1 Comment', '% Comments' );
 $get_comments_number_html = ob_get_clean(); ?>
 
-<?php //TODO: Need to remove font awesome, add through filter in child ?>
 <ul class="post-info">
-  <li class="post-date"><i class="fa fa-calendar"></i><?php apply_filters('post_info_date', the_time('F jS Y') ); ?></li>
-  <li class="post-author"><i class="fa fa-user"></i><?php apply_filters('post_info_author', the_author() ); ?></li>
-  <li class="post-comment-count"><i class="fa fa-comments"></i>
-    <a href="<?php get_comments_link() ?>" title="Comment on <?php the_title() ?>"><?php echo $get_comments_number_html ?></a>
-  </li>
+  <li class="post-date"><?php apply_filters('post_info_date', get_the_time('F jS Y') ); ?></li>
+  <li class="post-author"><?php apply_filters('post_info_author', get_the_author() ); ?></li>
+  <?php $comments_count = '<a href="'.get_comments_link().'" title="Comment on '.get_the_title().'">'.$get_comments_number_html.'</a>'; ?>
+  <li class="post-comment-count"><?php apply_filters('post_info_comments_count', $comments_count ); ?></li>
 </ul>
