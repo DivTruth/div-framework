0ef5009df6ad755076fbdb4db99312dce3d91e28
Nick Worth <nick@divtruth.com>
2014-12-02 03:16:04 -0700

    Add logo to customizer

--- partials/header/part-titlespace.php
+++ partials/header/part-titlespace.php
@@ -1,5 +1,6 @@
 <div id="header-top" class="clearfix">
 	<div id="logo-wrap">
+		<?php do_action( 'df_insert_logo' ); // Used by customizer ?>
 		<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
 		<p class="h1 logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
 		<!-- if you'd like to use the site description you can un-comment it below -->
